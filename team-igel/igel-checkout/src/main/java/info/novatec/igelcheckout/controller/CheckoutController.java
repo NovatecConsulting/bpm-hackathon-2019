912d17939eb6e1f075d4a3a44454321758403bcb
Ben Fuernrohr <benjamin.fuernrohr@novatec-gmbh.de>
2019-11-28 13:55:58 +0100

    Team-Igel: moved everything to docker-environment

--- team-igel/igel-checkout/src/main/java/info/novatec/igelcheckout/controller/CheckoutController.java
+++ team-igel/igel-checkout/src/main/java/info/novatec/igelcheckout/controller/CheckoutController.java
@@ -1,11 +1,12 @@
 package info.novatec.igelcheckout.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import info.novatec.igelcheckout.service.CheckoutService;
+import info.novatec.messages.OrderPlacedEvent;
 
 @RestController
 public class CheckoutController {
@@ -13,9 +14,9 @@ public class CheckoutController {
     @Autowired
     private CheckoutService checkoutService;
 
-    @PostMapping("v1/checkout/{id}")
-    public void checkout(@PathVariable String id) {
-        checkoutService.checkout(id);
+    @PostMapping("v1/checkout")
+    public void checkout(@RequestBody OrderPlacedEvent order) {
+        checkoutService.checkout(order);
     }
 
 }
--- team-igel/igel-checkout/src/main/java/info/novatec/igelcheckout/service/CheckoutService.java
+++ team-igel/igel-checkout/src/main/java/info/novatec/igelcheckout/service/CheckoutService.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import info.novatec.messages.OrderPlacedEvent;
+
 @Service
 public class CheckoutService {
 
     private static final String CHECKOUT_TOPIC = "orderPlaced";
 
     @Autowired
-    private KafkaTemplate<String, String> template;
+    private KafkaTemplate<String, OrderPlacedEvent> template;
 
-    public void checkout(String id) {
-        template.send(CHECKOUT_TOPIC, id, id);
+    public void checkout(OrderPlacedEvent order) {
+        template.send(CHECKOUT_TOPIC, order.getOrderId(), order);
     }
 
 }
