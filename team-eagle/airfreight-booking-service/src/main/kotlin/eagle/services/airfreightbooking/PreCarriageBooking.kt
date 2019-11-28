0ccfd95738ce7e3179531648c4c57a1e2d8660a5
Lars Duelfer <lars.duelfer@novatec-gmbh.de>
2019-11-28 16:25:57 +0100

    Team Eagle: Add airfreigh booking service

---
+++ team-eagle/airfreight-booking-service/src/main/kotlin/eagle/services/airfreightbooking/PreCarriageBooking.kt
@@ -0,0 +1,43 @@
+package eagle.services.precarriagebooking.eagle.services.airfreightbooking
+
+import io.zeebe.client.ZeebeClient
+import io.zeebe.client.api.worker.JobWorker
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+import javax.annotation.PreDestroy
+
+@Component
+class AirfreightBooking {
+    private lateinit var zeebeClient: ZeebeClient
+    private lateinit var jobWorker: JobWorker
+    private val jobType = "book-airfreight-transport"
+
+    @PostConstruct
+    fun connectAndSubscribe() {
+        zeebeClient = ZeebeClient.newClientBuilder()
+                .brokerContactPoint("127.0.0.1:26500")
+                .usePlaintext()
+                .build()
+
+        jobWorker = zeebeClient.newWorker()
+                .jobType(jobType)
+                .handler { jobClient, job ->
+                    LOGGER.info("Job " + jobType + " received")
+                    jobClient.newCompleteCommand(job.key)
+                            .send()
+                            .join()
+                }
+                .open()
+    }
+
+    @PreDestroy
+    fun close() {
+        jobWorker.close()
+        zeebeClient.close()
+    }
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(AirfreightBooking.javaClass)
+    }
+}
