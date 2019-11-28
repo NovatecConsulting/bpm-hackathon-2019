99b8189e0b890e81c9c3c986bd46a3855a93e311
Ben Fuernrohr <benjamin.fuernrohr@novatec-gmbh.de>
2019-11-28 16:23:32 +0100

    Team-Igel: connect order to zeebe

--- team-igel/igel-message-api/src/main/java/info/novatec/messages/OrderPlacedEvent.java
+++ team-igel/igel-message-api/src/main/java/info/novatec/messages/OrderPlacedEvent.java
@@ -40,4 +40,38 @@ public class OrderPlacedEvent {
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.amount;
+        result = prime * result + ((this.item == null) ? 0 : this.item.hashCode());
+        result = prime * result + ((this.orderId == null) ? 0 : this.orderId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderPlacedEvent other = (OrderPlacedEvent) obj;
+        if (this.amount != other.amount)
+            return false;
+        if (this.item == null) {
+            if (other.item != null)
+                return false;
+        } else if (!this.item.equals(other.item))
+            return false;
+        if (this.orderId == null) {
+            if (other.orderId != null)
+                return false;
+        } else if (!this.orderId.equals(other.orderId))
+            return false;
+        return true;
+    }
 }
