611f09353f3400de7dee9b1459100b7c14d69bb5
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2019-11-28 09:56:37 +0100

    MON: Empty App created by maven archetype

---
+++ monitoring/application/data-app/src/main/java/info/novatec/mon/CamundaApplication.java
@@ -0,0 +1,13 @@
+package info.novatec.mon;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication("data-app")
+public class CamundaApplication {
+  public static void main(String... args) {
+    SpringApplication.run(CamundaApplication.class, args);
+  }
+}
