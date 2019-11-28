99c5e80580d5865e21f1006a120e21f192914bcf
Ben Fuernrohr <benjamin.fuernrohr@novatec-gmbh.de>
2019-11-28 10:33:06 +0100

    Team-Igel: init checkout service, restructure rest

---
+++ team-igel/igel-checkout/src/main/java/info/novatec/igelcheckout/IgelCheckoutApplication.java
@@ -0,0 +1,13 @@
+package info.novatec.igelcheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgelCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgelCheckoutApplication.class, args);
+	}
+
+}
---
+++ team-igel/igel-checkout/src/test/java/info/novatec/igelcheckout/IgelCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package info.novatec.igelcheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgelCheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
