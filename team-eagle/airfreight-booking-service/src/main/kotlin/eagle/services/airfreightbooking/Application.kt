84b0ac343a40824dc62be8367cf0cbdea18bc318
Lars Duelfer <lars.duelfer@novatec-gmbh.de>
2019-11-28 17:55:05 +0100

    Team Eagle: Finalize version 1.0.0 of transport process example

--- team-eagle/airfreight-booking-service/src/main/kotlin/eagle/services/airfreightbooking/Application.kt
+++ team-eagle/airfreight-booking-service/src/main/kotlin/eagle/services/airfreightbooking/Application.kt
@@ -1,4 +1,4 @@
-package eagle.services.precarriagebooking
+package eagle.services.airfreightbooking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
